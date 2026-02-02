da3c5ee9f12bd70afeb304f99eb8fcf7a86b0118
shar <faethychan@gmail.com>
2026-02-03 04:17:23 +0800

    Add BoardGame class

---
+++ BoardGame.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include<string>
+
+
+class BoardGame
+{
+	int index;
+	std::string name;
+	int minPlayers;
+	int maxPlayers;
+	int minPlaytime;
+	int maxPlaytime;
+	int yearPublished;
+
+public:
+	BoardGame();
+	BoardGame(const std::string name, const int minPlayers, const int maxPlayers, const int minPlaytime, const int maxPlaytime, const int yearPublished);
+	void print();
+};
+
