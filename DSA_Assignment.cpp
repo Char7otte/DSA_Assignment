097380039ef4e033f2dd6249829584b30a90a9bc
shar <faethychan@gmail.com>
2026-02-03 04:18:14 +0800

    Add CSV data reading and parsing for board games
    Implemented the readDataFile function to read and parse board game data from a CSV file, handling quoted names and converting fields to appropriate types. Integrated this function into main, replacing the previous placeholder code.

--- DSA_Assignment.cpp
+++ DSA_Assignment.cpp
@@ -1,18 +1,64 @@
 #include <iostream>
 #include <fstream>
 #include<string>
+#include<sstream>
+#include<iomanip>
+
+#include "List.h"
+#include "BoardGame.h"
+#include "member.h"
+
+List readDataFile(std::string fileName);
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    
-    std::ifstream inputFile("games.csv");
-    std::string line;
+    List boardGamesList = readDataFile("games.csv");
+    return 0;
+}
+
+List readDataFile(std::string fileName) {
+    List newList;
+    std::cout << "Reading games.csv" << "\n";
+
+    std::ifstream inputFile(fileName);
+    std::string line, name, minPlayers, maxPlayers, minPlaytime, maxPlaytime, yearPublished;
+    std::getline(inputFile, line);
     while (std::getline(inputFile, line)) {
-        std::cout << line << "\n";
+        std::stringstream ss(line);
+
+        //In case the board game's name has a comma, quotes will be placed around the name.
+        if (ss.peek() == '"') {
+            ss >> std::quoted(name);
+            if (ss.peek() == ',') ss.ignore();
+        }
+        else {
+            std::getline(ss, name, ',');
+        }
+
+        std::getline(ss, minPlayers, ',');
+        std::getline(ss, maxPlayers, ',');
+        std::getline(ss, maxPlaytime, ',');
+        std::getline(ss, minPlaytime, ',');
+        std::getline(ss, yearPublished, ',');
+
+        BoardGame newBoardGame(
+            name,
+            std::stoi(minPlayers),
+            std::stoi(maxPlayers),
+            std::stoi(minPlaytime),
+            std::stoi(maxPlaytime),
+            std::stoi(yearPublished)
+        );
+
+        newList.add(newBoardGame);
+
     }
 
     inputFile.close();
+    newList.printAll();
 
-    return 0;
+    std::cout << "\n";
+    std::cout << "File read successfully." << "\n";
+
+    return newList;
 }
