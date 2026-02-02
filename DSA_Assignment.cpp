e64d5bf880ef3cced9ab5cae9b48032679442b87
shar <faethychan@gmail.com>
2026-02-03 02:35:13 +0800

    Read and print all lines from games.csv

--- DSA_Assignment.cpp
+++ DSA_Assignment.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include<string>
 
 int main()
 {
     std::cout << "Hello World!\n";
+    
+    std::ifstream inputFile("games.csv");
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::cout << line << "\n";
+    }
+
+    inputFile.close();
+
+    return 0;
 }
