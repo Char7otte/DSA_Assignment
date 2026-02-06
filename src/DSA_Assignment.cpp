36823975025341bc6c7bca5614561dc71b2ca13b
shar <faethychan@gmail.com>
2026-02-07 04:25:12 +0800

    Add option to print gamesDict to dashboards

--- src/DSA_Assignment.cpp
+++ src/DSA_Assignment.cpp
@@ -84,6 +84,7 @@ bool adminDashboard(GameDictionary& games) {
         std::cout << "2. Remove a board game" << "\n";
         std::cout << "3. Add a new member" << "\n";
         std::cout << "4. Display borrow history log" << "\n";
+        std::cout << "5. Display all board games" << "\n";
         std::cout << "0. Logout" << "\n";
         std::string input;
         std::getline(std::cin, input);
@@ -100,6 +101,9 @@ bool adminDashboard(GameDictionary& games) {
         else if (input == "4") {
             std::cout << "Display borrow history log";
         }
+        else if (input == "5") {
+            games.print();
+        }
         else if (input == "0") {
             break;
         }
@@ -115,6 +119,7 @@ bool memberDashboard(GameDictionary& games) {
         std::cout << "1. Borrow a board game" << "\n";
         std::cout << "2. Return a board game" << "\n";
         std::cout << "3. Add a new member" << "\n";
+        std::cout << "4. Display all board games" << "\n";
         std::cout << "0. Logout" << "\n";
         std::string input;
         std::getline(std::cin, input);
@@ -127,6 +132,9 @@ bool memberDashboard(GameDictionary& games) {
         }
         else if (input == "3") {
             std::cout << "Display your borrow history";
+        }
+        else if (input == "4") {
+            games.print();
         }
         else if (input == "0") {
             break;
