e6de748b15f065582101cc0c03d2cf8737415fa1
shar <faethychan@gmail.com>
2026-02-07 06:06:05 +0800

    Remove previous getDate function

--- src/DSA_Assignment.cpp
+++ src/DSA_Assignment.cpp
@@ -77,24 +77,6 @@ Member* login(MemberDictionary& members) {
     return foundAccount;
 }
 
-//
-//std::string getCurrentDate() {
-//    // 1. Get the current system time
-//    auto now = std::chrono::system_clock::now();
-//
-//    // 2. Convert to a time_t object (standard C time)
-//    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
-//
-//    // 3. Convert to local time structure
-//    std::tm* localTime = std::localtime(&currentTime);
-//
-//    // 4. Use stringstream to format it (YYYY-MM-DD)
-//    std::stringstream ss;
-//    ss << std::put_time(localTime, "%Y-%m-%d");
-//
-//    return ss.str();
-//}
-//
 //void memberBorrowMenu(GameDictionary& gameDict,
 //                      Member& selectedMember,
 //                      MasterHistoryLog& historyList) {
