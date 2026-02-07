abdeb09c9a4e48ac0bfa491ec12958e702774dab
shar <faethychan@gmail.com>
2026-02-07 23:10:42 +0800

    Read games.csv

--- src/FileUtilities.cpp
+++ src/FileUtilities.cpp
@@ -6,67 +6,69 @@
 #include<fstream>
 #include<sstream>
 
-//void readGameFile(const std::string& fileName, GameDictionary& gameDict) {
-//    std::cout << "Reading " << fileName << "\n";
-//
-//    std::ifstream inputFile(fileName);
-//    std::string line, name, minPlayers, maxPlayers, minPlaytime, maxPlaytime, yearPublished;
-//    std::getline(inputFile, line);
-//    int iteration = 1;
-//    while (std::getline(inputFile, line)) {
-//        std::stringstream ss(line);
-//
-//        //In case the board game's name has a comma, quotes will be placed around the name.
-//        if (ss.peek() == '"') {
-//            ss >> std::quoted(name);
-//            if (ss.peek() == ',') ss.ignore();
-//        }
-//        else {
-//            std::getline(ss, name, ',');
-//        }
-//        std::getline(ss, minPlayers, ',');
-//        std::getline(ss, maxPlayers, ',');
-//        std::getline(ss, maxPlaytime, ',');
-//        std::getline(ss, minPlaytime, ',');
-//        std::getline(ss, yearPublished, ',');
-//
-//        BoardGame* newBoardGame = new BoardGame(
-//            "G" + std::to_string(iteration),
-//            name,
-//            std::stoi(minPlayers),
-//            std::stoi(maxPlayers),
-//            std::stoi(minPlaytime),
-//            std::stoi(maxPlaytime),
-//            std::stoi(yearPublished)
-//        );
-//
-//        gameDict.add(std::to_string(iteration), newBoardGame);
-//        iteration++;
-//    }
-//
-//    inputFile.close();
-//    gameDict.print();
-//
-//    std::cout << "\n";
-//    std::cout << "File read successfully." << "\n" << "\n";
-//}
-
 void readGameFile(const std::string& fileName, GameDictionary& gameDict) {
     std::cout << "Reading " << fileName << "\n";
 
-    BoardGame* b1 = new BoardGame("G001", "12 Days", 3, 5, 15, 15, 2011);
-    BoardGame* b2 = new BoardGame("G002","Rat - a - Tat Cat", 2, 6, 10, 10, 1995);
-    BoardGame* b3 = new BoardGame("G003", "Yedo", 2, 5, 180, 120, 2012);
+    std::ifstream inputFile(fileName);
+    std::string line, name, minPlayers, maxPlayers, minPlaytime, maxPlaytime, yearPublished;
+    std::getline(inputFile, line);
+    int iteration = 1;
+    while (std::getline(inputFile, line)) {
+        std::stringstream ss(line);
 
-    gameDict.add("G001", b1);
-    gameDict.add("G002", b2);
-    gameDict.add("G003", b3);
+        //In case the board game's name has a comma, quotes will be placed around the name.
+        if (ss.peek() == '"') {
+            ss >> std::quoted(name);
+            if (ss.peek() == ',') ss.ignore();
+        }
+        else {
+            std::getline(ss, name, ',');
+        }
+        std::getline(ss, minPlayers, ',');
+        std::getline(ss, maxPlayers, ',');
+        std::getline(ss, maxPlaytime, ',');
+        std::getline(ss, minPlaytime, ',');
+        std::getline(ss, yearPublished, ',');
 
+        std::string id = "G" + std::to_string(iteration);
+
+        BoardGame* newBoardGame = new BoardGame(
+            id,
+            name,
+            std::stoi(minPlayers),
+            std::stoi(maxPlayers),
+            std::stoi(minPlaytime),
+            std::stoi(maxPlaytime),
+            std::stoi(yearPublished)
+        );
+
+        gameDict.add(id, newBoardGame);
+        iteration++;
+    }
+
+    inputFile.close();
     gameDict.print();
 
     std::cout << "\n";
-        std::cout << "File read successfully." << "\n" << "\n";
+    std::cout << "File read successfully." << "\n" << "\n";
 }
+
+//void readGameFile(const std::string& fileName, GameDictionary& gameDict) {
+//    std::cout << "Reading " << fileName << "\n";
+//
+//    BoardGame* b1 = new BoardGame("G001", "12 Days", 3, 5, 15, 15, 2011);
+//    BoardGame* b2 = new BoardGame("G002","Rat - a - Tat Cat", 2, 6, 10, 10, 1995);
+//    BoardGame* b3 = new BoardGame("G003", "Yedo", 2, 5, 180, 120, 2012);
+//
+//    gameDict.add("G001", b1);
+//    gameDict.add("G002", b2);
+//    gameDict.add("G003", b3);
+//
+//    gameDict.print();
+//
+//    std::cout << "\n";
+//        std::cout << "File read successfully." << "\n" << "\n";
+//}
 
 void readMemberFile(const std::string& fileName, MemberDictionary& memberDict) {
     std::cout << "Reading " << fileName << "\n";
