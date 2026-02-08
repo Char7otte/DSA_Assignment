43ebf57ce591f805f21c53e5b52cc3c456cf6e75
shar <faethychan@gmail.com>
2026-02-08 22:44:25 +0800

    test

--- src/DSA_Assignment.cpp
+++ src/DSA_Assignment.cpp
@@ -17,6 +17,13 @@ Member* login(MemberDictionary& members);
 
 
 int main() {
+    MatchLog testLog;
+    MatchList testList;
+    testList.add("1", "1", 1, "1", "1");
+    testList.print();
+
+
+
     GameDictionary gameDict;
     readGameFile("./data/games.csv", gameDict);
     MemberDictionary memberDict;
